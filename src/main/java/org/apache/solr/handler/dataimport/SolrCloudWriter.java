c2d392d3ccedb7ad240d70dd4ee891489da0ba56
Mikhail Khludnev <mkhl@apache.org>
2023-12-20 23:48:14 +0300

    extract method

--- src/main/java/org/apache/solr/handler/dataimport/SolrCloudWriter.java
+++ src/main/java/org/apache/solr/handler/dataimport/SolrCloudWriter.java
@@ -54,11 +54,10 @@ public class SolrCloudWriter extends SolrWriter /*weird but it seems it's worth 
         updateClient = req.getCoreContainer().getUpdateShardHandler().getUpdateOnlyHttpClient();
         destColl = req.getParams().get(DST_COLL_PARAM);
         if (destColl == null) {
-            throw new SolrException(SolrException.ErrorCode.BAD_REQUEST,
-                    SolrWriter.class.getCanonicalName() + " requires " + DST_COLL_PARAM);
+            throw new SolrException(SolrException.ErrorCode.BAD_REQUEST, SolrWriter.class.getCanonicalName() + " requires " + DST_COLL_PARAM);
         }
         CoreContainer cc = req.getCoreContainer();
-        CloudDescriptor cloudDesc = req.getCore().getCoreDescriptor().getCloudDescriptor();
+        //CloudDescriptor cloudDesc = req.getCore().getCoreDescriptor().getCloudDescriptor();
         ZkController zkController = cc.getZkController();
         ClusterState clusterState = zkController.getClusterState();
         destDocColl = clusterState.getCollection(destColl); // existential assert here
@@ -72,14 +71,19 @@ public class SolrCloudWriter extends SolrWriter /*weird but it seems it's worth 
             // can;t manage to commit via SolrCmdDistributor
             // List<SolrCmdDistributor.Node> collectionUrls = getCollectionUrls(destDocColl, EnumSet.of(Replica.Type.TLOG, Replica.Type.NRT), true);
             solrCmdDistributor.blockAndDoRetries();//distribCommit(commit, collectionUrls, new ModifiableSolrParams());
-            String baseUrl = destDocColl.getActiveSlicesArr()[0].getLeader().getBaseUrl();
-            UpdateRequest cmt = new UpdateRequest();
+            UpdateRequest cmt = updateAnyLeader();
             cmt.setAction(optimize ? UpdateRequest.ACTION.OPTIMIZE : UpdateRequest.ACTION.COMMIT, true, true);
-            cmt.setBasePath(baseUrl);
             cmt.process(updateClient, destColl);
         } catch (Exception e) {
             log.warn("Error commit back: ", e);
         }
+    }
+
+    private UpdateRequest updateAnyLeader() {
+        UpdateRequest cmt = new UpdateRequest();
+        String baseUrl = destDocColl.getActiveSlicesArr()[0].getLeader().getBaseUrl();
+        cmt.setBasePath(baseUrl);
+        return cmt;
     }
 
 
@@ -96,10 +100,8 @@ public class SolrCloudWriter extends SolrWriter /*weird but it seems it's worth 
     public void rollback() {
         try {
             solrCmdDistributor.blockAndDoRetries();// but i'd rather nuke'em
-            String baseUrl = destDocColl.getActiveSlicesArr()[0].getLeader().getBaseUrl();
-            UpdateRequest cmt = new UpdateRequest();
+            UpdateRequest cmt = updateAnyLeader();
             cmt.rollback();
-            cmt.setBasePath(baseUrl);
             cmt.process(updateClient, destColl);
         } catch (Exception e) {
             log.warn("Error rolling back: ", e);
@@ -111,10 +113,8 @@ public class SolrCloudWriter extends SolrWriter /*weird but it seems it's worth 
         try {
             //updateClient.deleteByQuery(destColl, q, commitWithin);
             solrCmdDistributor.blockAndDoRetries();
-            String baseUrl = destDocColl.getActiveSlicesArr()[0].getLeader().getBaseUrl();
-            UpdateRequest cmt = new UpdateRequest();
+            UpdateRequest cmt = updateAnyLeader();
             cmt.deleteByQuery(q); // TODO pass commitWithin
-            cmt.setBasePath(baseUrl);
             cmt.process(updateClient, destColl);
         } catch (Exception e) {
             log.warn("Error deleting by: " + q, e);
@@ -125,10 +125,8 @@ public class SolrCloudWriter extends SolrWriter /*weird but it seems it's worth 
     public void doDeleteAll() {
         try {
             solrCmdDistributor.blockAndDoRetries();// but i'd rather nuke'em
-            String baseUrl = destDocColl.getActiveSlicesArr()[0].getLeader().getBaseUrl();
-            UpdateRequest cmt = new UpdateRequest();
+            UpdateRequest cmt = updateAnyLeader();
             cmt.deleteByQuery("*:*"); // TODO pass commitWithin
-            cmt.setBasePath(baseUrl);
             cmt.process(updateClient, destColl);
         } catch (Exception e) {
             log.warn("Error deleting all docs : ", e);
@@ -140,10 +138,8 @@ public class SolrCloudWriter extends SolrWriter /*weird but it seems it's worth 
         try {
             updateClient.deleteById(destColl, key.toString(), commitWithin);
             solrCmdDistributor.blockAndDoRetries();
-            String baseUrl = destDocColl.getActiveSlicesArr()[0].getLeader().getBaseUrl();
-            UpdateRequest cmt = new UpdateRequest();
+            UpdateRequest cmt = updateAnyLeader();
             cmt.deleteById(key.toString()); // TODO pass commitWithin
-            cmt.setBasePath(baseUrl);
             cmt.process(updateClient, destColl);
         } catch (Exception e) {
             log.warn("Error deleting document : " + key, e);
@@ -154,12 +150,11 @@ public class SolrCloudWriter extends SolrWriter /*weird but it seems it's worth 
     public boolean upload(SolrInputDocument doc) {
         AddUpdateCommand command = new AddUpdateCommand(req);// accesses schema !
         command.solrDoc = doc;
-
         Slice slice = destDocColl.getRouter().getTargetSlice(command.getIndexedIdStr(), doc, null, req.getParams(), destDocColl);
-
         try {
-            solrCmdDistributor.distribAdd(command, Collections.singletonList(
-                    new SolrCmdDistributor.StdNode(new ZkCoreNodeProps(slice.getLeader()), destColl, slice.getName())), new ModifiableSolrParams(Map.of("commitWithin", new String[]{Integer.toString(commitWithin)})));
+            SolrCmdDistributor.StdNode shardLeaderNode = new SolrCmdDistributor.StdNode(new ZkCoreNodeProps(slice.getLeader()), destColl, slice.getName());
+            ModifiableSolrParams commitWithinParam = new ModifiableSolrParams(Map.of("commitWithin", new String[]{Integer.toString(commitWithin)}));
+            solrCmdDistributor.distribAdd(command, Collections.singletonList(shardLeaderNode), commitWithinParam);
             return true;
         } catch (Exception e) {
             log.warn("Error creating document : " + doc, e);
